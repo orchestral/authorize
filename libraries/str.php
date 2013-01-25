40fd705697354f1f63f775b27a5e1cabbb99ee54
crynobone <crynobone@gmail.com>
2013-01-25 09:33:47 +0800

    Add docblock to Authorize\Str::humanize()
    Signed-off-by: crynobone <crynobone@gmail.com>

--- libraries/str.php
+++ libraries/str.php
@@ -4,6 +4,14 @@ use Laravel\Str as S;
 
 class Str extends S {
 	
+	/**
+	 * Convert slug type text to human readable text.
+	 *
+	 * @static
+	 * @access public
+	 * @param  string   $text
+	 * @return string
+	 */
 	public static function humanize($text)
 	{
 		return static::title(str_replace(array('-', '_'), ' ', $text));
