3b50a29d297e17ce08975236d2eaf3aa1e68b5bf
crynobone <crynobone@gmail.com>
2013-01-27 00:41:14 +0800

    Whitespaces.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/roles/index.blade.php
+++ views/roles/index.blade.php
@@ -7,11 +7,13 @@
 		<div class="pull-right">
 			<a href="{{ URL::current() }}/view" class="btn btn-primary">Add</a>
 		</div>
+		
 		<h2>{{ isset($_title_) ? $_title_ : 'Authorize' }}
 			@if ( ! empty($_description_))
 			<small>{{ $_description_ ?: '' }}</small>
 			@endif
 		</h2>
+
 	</div>
 
 	{{ $table }}
