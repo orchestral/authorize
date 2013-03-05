3b209debe3d1e7a5dbd702889065581625362ace
crynobone <crynobone@gmail.com>
2013-03-05 17:51:56 +0800

    Orchestra\Platform 1.1.2 updates.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/roles/index.blade.php
+++ views/roles/index.blade.php
@@ -1,28 +1,7 @@
 @include(locate('authorize::widgets.menu'))
 
 <div class="row-fluid">
-	
-	<div class="page-header">
-
-		<div class="pull-right">
-			<a href="{{ URL::current() }}/view" class="btn btn-primary">Add</a>
-		</div>
-		
-		<?php
-
-		$title       = Orchestra\Site::get('title');
-		$description = Orchestra\Site::get('description'); ?>
-
-		<div class="page-header">
-			<h2>{{ $title ?: 'Authorize' }}
-				@if ( ! empty($description))
-				<small>{{ $description ?: '' }}</small>
-				@endif
-			</h2>
-		</div>
-
-	</div>
-
+	<?php Orchestra\Site::set('header::add-button', true); ?>
+	@include(locate('orchestra::layout.widgets.header'))
 	{{ $table }}
-
 </div>
