f27c90cdfcd2133b0ed5dc8a2b2ca2c9240f7425
crynobone <crynobone@gmail.com>
2013-03-08 16:28:17 +0800

    Update navigation to use Orchestra\Decorator::navbar()
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/widgets/menu.blade.php
+++ views/widgets/menu.blade.php
@@ -1,27 +1,26 @@
-<div class="navbar">
-	<div class="navbar-inner">
+@section('authorize::primary_menu')
+<ul class="nav">
+	@if (Orchestra::acl()->can('manage-roles'))
+	<li class="{{ URI::is('*/resources/authorize.roles*') ? 'active' : '' }}">
+		{{ HTML::link(handles('orchestra::resources/authorize.roles'), 'Roles') }}
+	</li>
+	@endif
+	@if (Orchestra::acl()->can('manage-acl'))
+	<li class="{{ URI::is('*/resources/authorize.acls*') ? 'active' : '' }}">
+		{{ HTML::link(handles('orchestra::resources/authorize.acls'), 'ACL') }}
+	</li>
+	@endif
+</ul>
+@endsection
 
-		<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target="#authorizenav">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
 
-		{{ HTML::link(handles('orchestra::resources/authorize'), 'Authorize', array('class' => 'brand')) }}
+<?php
 
-		<div id="authorizenav" class="collapse nav-collapse">
-		  	<ul class="nav">
-		  		@if (Orchestra::acl()->can('manage-roles'))
-				<li class="{{ URI::is('*/resources/authorize.roles*') ? 'active' : '' }}">
-					{{ HTML::link(handles('orchestra::resources/authorize.roles'), 'Roles') }}
-				</li>
-				@endif
-				@if (Orchestra::acl()->can('manage-acl'))
-				<li class="{{ URI::is('*/resources/authorize.acls*') ? 'active' : '' }}">
-					{{ HTML::link(handles('orchestra::resources/authorize.acls'), 'ACL') }}
-				</li>
-				@endif
-			</ul>
-		</div>
-	</div>
-</div>
+$navbar = new Orchestra\Fluent(array(
+	'id'             => 'authorize',
+	'title'          => 'Authorize',
+	'url'            => handles('orchestra::resources/authorize'),
+	'primary_menu'   => Laravel\Section::yield('authorize::primary_menu'),
+)); ?>
+
+{{ Orchestra\Decorator::navbar($navbar) }}
